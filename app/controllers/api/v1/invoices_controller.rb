f6ea8acae19476d866a0d8c89cf3fa9437a01e46
François Bulteau <bulteau@MacBook-Pro-de-Francois-2.local>
2018-02-27 00:44:00 +0100

    add some comments

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::InvoicesController < Api::V1::BaseController
+
   def index
     respond_with Invoice.all
   end
 
+  #Generate an invoice from a contrat monthly rate, without save in database
   def simulator
     @invoice = Invoice.new(:contract_monthly_rent => params[:invoice][:contract_monthly_rent])
     render json: @invoice
